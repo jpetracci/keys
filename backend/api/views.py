b1f4733007bbc2f6295083ae0c5528d4e1c696e5
Joel Petracci <jpetracci@gmail.com>
2024-05-23 16:49:11 -0700

    added some views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TransactionSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Transaction
 
-# Create your views here.
+
+
+class TransactionListCreate(generics.ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(author=user)
+    
+    def perform_create(self, serialzer):
+        if serialzer.is_valid():
+            serialzer.save(author=self.request.user)
+        else:
+            print(serialzer.errors)
+
+class TransactionDelete(generics.DestroyAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(author=user)
+
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
